f84fd66fbe141b0917a7b4771c7007da749c514e
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-07-12 16:13:24 +0200

    message error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-import { Mailgun } from 'mailgun';
-
 var typed = new Typed(".typing", {
   strings: ['web Designer', 'Graphic designer', 'Web Developer',],
   typedSpeed: 100,
@@ -29,27 +27,12 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
 });
 
 function sendEmail() {
+  const names = document.getElementById('names').value;
+  const email = document.getElementById('email').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
 
-  var mg = new Mailgun('2536ce22a998fc4065f0b5b547ec38c2-8a084751-c5a69077');
-  mg.sendText('uwingajoselyne@gmail.com', [document.getElementById('email').value],
-    'This is the subject',
-    'This is the text',
-    function (err) {
-      if (err) console.log('Oh noes: ' + err);
-      else console.log('Success');
-    });
-  // Email.send({
-  //   Host: "smtp.mailgun.org",
-  //   Username: "uwingajoselyne@gmail.com",
-  //   Password: "joselyne11",
-  //   To: document.getElementById('email').value,
-  //   From: "uwingajoselyne@gmail.com",
-  //   Subject: document.getElementById('subject').value,
-  //   Body: `Name: ${document.getElementById('name').value}
-  //              Email: ${document.getElementById('email').value}
-  //              Subject: ${document.getElementById('subject').value}
-  //              Message: ${document.getElementById('message').value}`
-  // }).then(
-  //   message => alert(message)
-  // );
+document.querySelectorAll("form span").forEach((el)=>{
+  el.style.display ="none"
+})
 }
